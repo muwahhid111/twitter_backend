219a6369d07264b9de4943afb9d8d62cfce33ffc
Yanarkaev <110416456+Yanarkaev@users.noreply.github.com>
2022-09-13 11:15:29 +0300

    Update posts.controller.js

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -17,13 +17,14 @@ module.exports.postsController = {
   likePost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+      const user = await User.findById(req.body.user);
       if (!post.likes.includes(req.body.user)) {
         await post.updateOne({ $push: { likes: req.body.user } });
-        await User.updateOne({ $push: { liked: post._id } });
+        await user.updateOne({ $push: { liked: post._id } });
         res.json("Пост лайкнут");
       } else {
         await post.updateOne({ $pull: { likes: req.body.user } });
-        await User.updateOne({ $pull: { liked: post._id } });
+        await user.updateOne({ $pull: { liked: post._id } });
         res.json("лайк снят");
       }
     } catch (error) {
